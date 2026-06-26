8338fd0ca77485a41af156d2b514ad0ab9c32be8
arnd25 <and30206@gmail.com>
2026-06-26 14:14:45 +0300

    правки

--- BackPP/src/modules/users/dto/create-user.dto.ts
+++ BackPP/src/modules/users/dto/create-user.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsEnum, IsString, MaxLength, MinLength } from 'class-validator';
-import { Role } from 'src/generated/prisma/enums';
+import { Role } from 'src/generated/prisma';
 
 export class CreateUserDto {
   @IsEmail({}, { message: 'Введите корректный email' })
